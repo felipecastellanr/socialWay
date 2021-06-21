064859d59eae5a9a2d670b0762d69b2bb2421d5c
felipecastellanr <felipecastellannn@hotmail.com>
2021-06-20 22:52:22 -0300

    criação tela comentarios e ajuste no historico

--- src/app/pages/comentarios/comentarios.page.ts
+++ src/app/pages/comentarios/comentarios.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-comentarios',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComentariosPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private modalCtrl: ModalController
+  ) { }
 
   ngOnInit() {
+  }
+
+  close(){
+    this.modalCtrl.dismiss();
   }
 
 }
--- src/app/pages/estab/estab.page.ts
+++ src/app/pages/estab/estab.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ComentariosPage } from '../comentarios/comentarios.page';
 
 @Component({
   selector: 'app-estab',
@@ -7,7 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstabPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private modalCtrl: ModalController
+  ) { }
+
+  async showModalComentario(){
+    const modal = await this.modalCtrl.create({
+      component: ComentariosPage
+    })
+    modal.present();
+  }
 
   ngOnInit() {
   }
